cfc368e801b3340cb01f8d61eff8bf04e3571f0a
Jared Addison <77221122+jaredaddison@users.noreply.github.com>
2021-08-19 11:05:37 +0800

    Initial commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+    <nav-bar />
+    <router-view />
+    <vue-footer />
+    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
+      Veterinary Education and Training
+    </div>
+  </main>
+</template>
